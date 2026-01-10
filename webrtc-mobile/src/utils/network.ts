88f6371dced94cea6d60e0871a92a368efc4bd17
Aryan Verma <aryan.verma@adypu.edu.in>
2026-01-10 14:16:08 +0530

    Expo backend

--- webrtc-mobile/src/utils/network.ts
+++ webrtc-mobile/src/utils/network.ts
@@ -2,6 +2,11 @@ import Constants from 'expo-constants';
 import { Platform } from 'react-native';
 
 export function getWsUrl(port: number) {
+  const backendUrl = (process.env as any).EXPO_PUBLIC_BACKEND_URL || "https://react-native-playground-backend.onrender.com";
+  if (backendUrl) {
+    return backendUrl.replace(/^http/, 'ws');
+  }
+
   const debuggerHost = Constants.expoConfig?.hostUri;
 
   if (debuggerHost) {
